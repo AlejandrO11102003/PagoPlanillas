b5e6730eacd0e2ba815eac4e021cadd934e17ae0
AlejandrO11102003 <u22239328@utp.edu.pe>
2026-08-17 20:09:31 -0500

    fix: fixx

--- planilla.test.js
+++ planilla.test.js
@@ -1,9 +1,9 @@
 const { calcularSueldo } = require('./planilla');
 
 test('Calcula correctamente el sueldo neto', () => {
-  expect(calcularSueldo(1500, 150)).toBe(1350);
+    expect(calcularSueldo(1500, 150)).toBe(1350);
 });
 
 test('Lanza un error si hay valores negativos', () => {
-  expect(() => calcularSueldo(-100, 50)).toThrow();
+    expect(() => calcularSueldo(-100, 50)).toThrow();
 });
